c5023b9500a3bd2bc3e2b1f989600097e8c4e3ec
Hadeel Murshed <131537843+Hadeel-Mur@users.noreply.github.com>
2023-12-18 07:50:06 +0100

    context

---
+++ src/context/GlobalState.js
@@ -0,0 +1,25 @@
+import React, {createContext, useReducer} from 'react';
+import AppReducer from './AppReducer'
+
+// Initial state
+const initialState = {
+    transactions: [
+        { id: 1, text: 'Flower', amount: -20 },
+        { id: 1, text: 'Flower', amount: -20 },
+        { id: 1, text: 'Flower', amount: -20 },
+
+    ]
+}
+
+// Create context
+export const GlobalContext = createContext(initialState)
+
+// Provider component
+export const GlobalProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AppReducer, initialState)
+  return (<GlobalContext.Provider value={{
+    transactions: state.transactions
+  }}>
+    {children}
+  </GlobalContext.Provider>)
+}
